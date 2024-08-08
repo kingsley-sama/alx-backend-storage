ac54d0a29f2c4fd7ac6ee31e84a9cdd08a50ca99
Kingsley Okpo <kngsley2018@gmail.com>
2024-08-09 00:55:28 +0100

    updates 0-uniq_users.sql ad 1-country_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,6 @@
 -- script that creates a table users if not already exist
-CREATE TABLE IF NOT EXISTS users (
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE users (
 id INT PRIMARY KEY NOT NULL,
 email VARCHAR(255) NOT NULL UNIQUE,
 name VARCHAR(255)
