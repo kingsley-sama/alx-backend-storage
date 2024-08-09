f6d62e4ed1b8af180af1876bf32f7665c8e9e733
Kingsley Okpo <kngsley2018@gmail.com>
2024-08-09 01:05:20 +0100

    updates 0-uniq_users.sql 2-fans.sql 1-country_users.sql

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,4 +1,4 @@
-/* script that creates a table users if not already exist */
+-- script that creates a table users if not already exist
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
 	   `id` int PRIMARY KEY NOT NULL AUTO_INCREAMENT,
