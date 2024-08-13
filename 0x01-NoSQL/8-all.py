e119f5581fb954330829ce5875cb0da2ed6a24d7
Kingsley Okpo <kngsley2018@gmail.com>
2024-08-13 18:12:06 +0100

    2-insert

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 import pymongo
+from pymongo import MongoClient
+"""inserts a file into a databases"""
 
+
+
+cluster = MongoClient("mongodb://localhost:27017")
+db = cluster["my_db"]
+collection = db["school"]
+post = {"name": "Holberton school"}
+collection.insert_one(post)
