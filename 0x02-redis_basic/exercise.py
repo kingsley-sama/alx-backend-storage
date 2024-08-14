28d45d4da4bb328c74e2f28bb3cf8380b82467b5
Kingsley Okpo <kngsley2018@gmail.com>
2024-08-14 22:40:28 +0100

    3

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,19 +1,140 @@
 #!/usr/bin/env python3
-"""This module defines the exercise.py class."""
+
+from typing import Callable, Optional, Union
+from uuid import uuid4
 import redis
-import uuid
-from typing import Union
+from functools import wraps
+
+'''
+    Writing strings to Redis.
+'''
+
+
+def count_calls(method: Callable) -> Callable:
+    '''
+        Counts the number of times a method is called.
+    '''
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        '''
+            Wrapper function.
+        '''
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """ Decorator to store the history of inputs and
+    outputs for a particular function.
+    """
+    key = method.__qualname__
+    inputs = key + ":inputs"
+    outputs = key + ":outputs"
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):  # sourcery skip: avoid-builtin-shadow
+        """ Wrapper for decorator functionality """
+        self._redis.rpush(inputs, str(args))
+        data = method(self, *args, **kwargs)
+        self._redis.rpush(outputs, str(data))
+        return data
+
+    return wrapper
+
+
+def replay(method: Callable) -> None:
+    # sourcery skip: use-fstring-for-concatenation, use-fstring-for-formatting
+    """
+    Replays the history of a function
+    Args:
+        method: The function to be decorated
+    Returns:
+        None
+    """
+    name = method.__qualname__
+    cache = redis.Redis()
+    calls = cache.get(name).decode("utf-8")
+    print("{} was called {} times:".format(name, calls))
+    inputs = cache.lrange(name + ":inputs", 0, -1)
+    outputs = cache.lrange(name + ":outputs", 0, -1)
+    for i, o in zip(inputs, outputs):
+        print("{}(*{}) -> {}".format(name, i.decode('utf-8'),
+                                     o.decode('utf-8')))
 
 
 class Cache:
-    """Create a store method that takes a data argument and returns a string"""
+    '''
+        Cache class.
+    '''
     def __init__(self):
+        '''
+            Initialize the cache.
+        '''
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """store method that takes a data argument and returns a string"""
-        id = uuid.uuid4().__str__()
-        self._redis.set(id, data)
+        '''
+            Store data in the cache.
+        '''
+        randomKey = str(uuid4())
+        self._redis.set(randomKey, data)
+        return randomKey
 
-        return id
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        '''
+            Get data from the cache.
+        '''
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        '''
+            Get a string from the cache.
+        '''
+        value = self._redis.get(key)
+        return value.decode('utf-8')
+
+    def get_int(self, key: str) -> int:
+        '''
+            Get an int from the cache.
+        '''
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode('utf-8'))
+        except Exception:
+            value = 0
+        return value
+# #!/usr/bin/env python3
+# """This module defines the exercise.py class."""
+# import redis
+# import uuid
+# from typing import Union, Callable
+
+
+# class Cache:
+#     """Create a store method that takes a
+#     def __init__(self):
+#         self._redis = redis.Redis()
+#         self._redis.flushdb()
+
+#     def store(self, data: Union[str, bytes, int, float]) -> str:
+#         """store method that takes a data argument and returns a string"""
+#         id = uuid.uuid4().__str__()
+#         self._redis.set(id, data)
+#         return id
+
+#     def get(self, key: str, fn: Callable = None) -> str:
+#         """gets a value using a key"""
+#         if callable == None:
+#             return(self._redis.get(key))
+#         else:
+#             return(self._redis.get(key).__fn__())
